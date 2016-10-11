f6910aa0cf56cb5d5e8787cdd9c972b5ad3e5d82
destanton <delisestanton@gmail.com>
2016-10-11 17:43:51 -0400

    got number operations working

--- django_calculator/app/views.py
+++ django_calculator/app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 
 
+def drop_down(num_input, num_input2, choice):
+        if choice == "Add":
+            return int(num_input) + int(num_input2)
+        elif choice == "Subtract":
+            return int(num_input) - int(num_input2)
+        elif choice == "Multiply":
+            return int(num_input) * int(num_input2)
+        elif choice == "Divide":
+            return int(num_input) / int(num_input2)
+
+
 def index_view(request):
-    print(request.POST)
-    
-    return render(request, "index.html")
+    # print(request.POST)
+    if request.GET:
+        num_input = request.GET["num_input"]
+        num_input2 = request.GET["num_input2"]
+        operator = request.GET["operator"]
+        print(request.GET)
+    else:
+        num_input = 1
+        num_input2 = 1
+        operator = "Add"
+    context = {
+        "output": drop_down(num_input, num_input2, operator)
+    }
+    return render(request, "index.html", context)
