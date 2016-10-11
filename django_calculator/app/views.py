59856af6e8185cda682e3036c41f2b2a3248924b
destanton <delisestanton@gmail.com>
2016-10-11 18:14:07 -0400

    fixed some variable tags and bugs

--- django_calculator/app/views.py
+++ django_calculator/app/views.py
@@ -2,28 +2,32 @@ from django.shortcuts import render
 
 
 def drop_down(num_input, num_input2, choice):
-        if choice == "Add":
+        if choice == "+":
             return int(num_input) + int(num_input2)
-        elif choice == "Subtract":
+        elif choice == "-":
             return int(num_input) - int(num_input2)
-        elif choice == "Multiply":
+        elif choice == "&times;":
             return int(num_input) * int(num_input2)
-        elif choice == "Divide":
+        elif choice == "&divide;":
             return int(num_input) / int(num_input2)
 
 
 def index_view(request):
     # print(request.POST)
-    if request.GET:
-        num_input = request.GET["num_input"]
-        num_input2 = request.GET["num_input2"]
-        operator = request.GET["operator"]
-        print(request.GET)
-    else:
-        num_input = 1
-        num_input2 = 1
-        operator = "Add"
-    context = {
-        "output": drop_down(num_input, num_input2, operator)
-    }
-    return render(request, "index.html", context)
+        if request.GET != "" or request.GET == int:
+            if request.GET:
+                num_input = request.GET["num_input"]
+                num_input2 = request.GET["num_input2"]
+                operator = request.GET["operator"]
+                print(request.GET)
+            else:
+                num_input = 1
+                num_input2 = 1
+                operator = "+"
+        context = {
+            "num": (num_input),
+            "oper": (operator),
+            "num2": (num_input2),
+            "output": drop_down(num_input, num_input2, operator)
+        }
+        return render(request, "index.html", context)
