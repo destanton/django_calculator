af6a82d930e0ed47fa0d94ca32e96d43af040d95
destanton <delisestanton@gmail.com>
2016-10-12 09:10:23 -0400

    fixed a bug with multiply and divide

--- django_calculator/app/views.py
+++ django_calculator/app/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render
 
 
-def drop_down(num_input, num_input2, choice):
+def drop_down(num_input, choice, num_input2):
         if choice == "+":
             return int(num_input) + int(num_input2)
         elif choice == "-":
             return int(num_input) - int(num_input2)
-        elif choice == "&times;":
+        elif choice == "*":
             return int(num_input) * int(num_input2)
-        elif choice == "&divide;":
+        elif choice == "/":
             return int(num_input) / int(num_input2)
 
 
 def index_view(request):
     # print(request.POST)
-        if request.GET != "" or request.GET == int:
-            if request.GET:
-                num_input = request.GET["num_input"]
-                num_input2 = request.GET["num_input2"]
-                operator = request.GET["operator"]
-                print(request.GET)
-            else:
-                num_input = 1
-                num_input2 = 1
-                operator = "+"
+        # if request.GET != "" or request.GET == int:
+        if request.GET:
+            num_input = request.GET["num_input"]
+            num_input2 = request.GET["num_input2"]
+            operator = request.GET["operator"]
+            print(request.GET)
+        else:
+            num_input = 1
+            num_input2 = 1
+            operator = "+"
         context = {
             "num": (num_input),
             "oper": (operator),
             "num2": (num_input2),
-            "output": drop_down(num_input, num_input2, operator)
+            "output": drop_down(num_input, operator, num_input2)
         }
         return render(request, "index.html", context)
