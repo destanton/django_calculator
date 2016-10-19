501b4c6f0c54015835bc18c4016eded651ef51d2
destanton <delisestanton@gmail.com>
2016-10-19 13:55:21 -0400

    added create user and login/logout

--- django_calculator/app/views.py
+++ django_calculator/app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
 
 
 def drop_down(num_input, choice, num_input2):
--- django_calculator/django_calculator/urls.py
+++ django_calculator/django_calculator/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from app.views import index_view
+from app.views import index_view, UserCreateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index_view, name="index_view")
+    url(r'^$', index_view, name="index_view"),
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view")
 ]
